eb2421fa674aef0b74daa7cac4fb9e2313598f01
YahyaAzzam <96234204+YahyaAzzam@users.noreply.github.com>
2023-05-28 12:19:14 +0300

    Radix sort fixed

--- Radix.java
+++ Radix.java
@@ -5,11 +5,18 @@ import java.util.Queue;
 public class Radix implements Sort{
 
     public int[][] sort(int[] array) {
-        int place = 1, max = 0;
+        int place = 1, max = 0, n = 0;
         for (int k : array) {
-            max = Math.max(max, k);
+            max = Math.max(max, Math.abs(k));
+            if (k < 0) n++;
         }
         max = (int) Math.log10(max) + 1;
+        int[] negative = new int[n], positive = new int[array.length-n];
+        int a = 0, b = 0;
+        for (int k : array) {
+            if (k < 0) negative[a++] = k;
+            else positive[b++] = k;
+        }
         int[][] intermediate = new int[max+1][array.length];
         intermediate[0] = array.clone();
         ArrayList<Queue<Integer>> buckets = new ArrayList<>();
@@ -18,16 +25,31 @@ public class Radix implements Sort{
         }
         for (int i = 0; i < max; i++) {
             int next = 0;
-            for (int j : array) {
+            for (int j : positive) {
                 buckets.get((j / (int) Math.pow(10, i)) % 10).add(j);
             }
             for (int j = 0; j < 10; j++) {
                 while (!buckets.get(j).isEmpty()) {
-                    array[next++] = buckets.get(j).remove();
+                    positive[next++] = buckets.get(j).remove();
                 }
             }
-            intermediate[place++] = array.clone();
+            next = 0;
+            for (int j : negative) {
+                buckets.get(((-j) / (int) Math.pow(10, i)) % 10).add(j);
+            }
+            for (int j = 0; j < 10; j++) {
+                while (!buckets.get(j).isEmpty()) {
+                    negative[next++] = buckets.get(j).remove();
+                }
+            }
+            for (int k : negative) {
+                intermediate[place][--next] = k;
+            }
+            System.arraycopy(positive, 0, intermediate[place], n, positive.length);
+            place++;
         }
+        System.arraycopy(negative, 0, array, 0, n);
+        System.arraycopy(positive, 0, array, n, positive.length);
         return intermediate;
     }
 }
